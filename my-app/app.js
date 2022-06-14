785439a9c41ce318dbf2788dff78d10c0f1773e3
OctoPol7 <thechumpower@gmail.com>
2022-06-13 21:02:19 -0700

    feat: add a user to the db

--- my-app/app.js
+++ my-app/app.js
@@ -5,7 +5,7 @@ const db = require('./db/connection.js');
 
 db.once('open', ()=>{
     console.log("Connected to database!");
-    const server = app.listen(8080, ()=>console.log("listening"));
+    const server = app.listen(3000, ()=>console.log("listening"));
 });
 
 app.use(express.static("public"));
@@ -15,16 +15,37 @@ const { User } = require('./models/User.js')
 
 app.post('/users', (req, res) => {
     let user = new User(req.body)
+    console.log('nice job!!')
+
+    user.save(error=>{
+        if(error) {
+            res.send(500).json(error)
+        } else {
+            res.status(201).json({
+                message: "Successfully Signed Up",
+                data: user
+            })
+        }
+    })
 })
 
-app.get('/', (req,res)=>{
- 
-    res.sendFile('index.html', {root: __dirname})
+app.get('/users', (req, res) => {
+    let dbQuery = User.find({})
 
-});
-
-app.get('/signup', (req,res)=>{
- 
-    res.sendFile('index.html', {root: __dirname})
-
-});
+    if(req.query.userName != undefined){
+        dbQuery = User.find({"unserName": req.query.userName})
+    } else if (req.query.userType != undefined) {
+        dbQuery = User.find({"userType": req.query.userType})
+    }
+    
+    dbQuery
+    .sort('userName')
+    .select('userName email')
+    .exec((error, result) => {
+        if(error) {
+            res.send(500).json(error)
+        } else {
+            res.json(result)
+        }
+    })
+})
