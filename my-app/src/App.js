6618dee8245f0411e133dad444602d565877b113
Saurabh <37953860+saurabhsindhu96@users.noreply.github.com>
2022-06-18 14:34:42 -0700

    Correction

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import Creatorpage from './CreatorPage/Creatorpage.js';
-import SignUp from './SignUp/SignUp.js'
-import LogIn from './LogIn/LogIn.js'
+import React from "react";
+import Creatorpage from "./CreatorPage/Creatorpage.js";
+import SignUp from "./SignUp/SignUp.js";
+import LogIn from "./LogIn/LogIn.js";
 
-const App = props => {
+const App = (props) => {
   return (
     <div className="App">
       <h1>InfluenSearch</h1>
-      <SignUp /
-
+      <SignUp />
 
       <LogIn />
-    
-      <Creatorpage />
 
+      <Creatorpage />
     </div>
   );
-}
+};
 
 export default App;
