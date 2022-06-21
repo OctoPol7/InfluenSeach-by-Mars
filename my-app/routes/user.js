122462e5f00b5b47abde618ba1297d29c815fb3f
Saurabh <37953860+saurabhsindhu96@users.noreply.github.com>
2022-06-20 17:14:25 -0700

    Signup
    Created username Routes and schema

--- my-app/routes/user.js
+++ my-app/routes/user.js
@@ -1,52 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 // code obtained from youtube course follow along
-// Building a RESTful API with Node.js 
+// Building a RESTful API with Node.js
 // https://www.youtube.com/playlist?list=PL55RiY5tL51q4D-B63KBnygU6opNPFk_q
-router.post('/signup', (req, res, next) => {
-    User.find({ email: req.body.email })
-      .exec()
-      .then(user => {
-        if (user.length >= 1) {
-            return res.status(409).json({
-                message: 'Email already exists!'
+router.post("/signup", (req, res, next) => {
+  User.find({ email: req.body.email, userName: req.body.userName })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Email already exists!",
+        });
+      }
+      //   if (user.length >= 1) {
+      //     return res.status(409).json({
+      //       message: "UserName already exists!",
+      //     });
+      //   }
+      else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
             });
-        } else {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        error: err
-                    });
-                } else {
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash
-                    });
-                    user
-                      .save()
-                      .then(result => {
-                        console.log(result);
-                        res.status(201).json({
-                            message: 'User created'
-                        });
-                      })
-                      .catch(err => {
-                        console.log(err);
-                        res.status(500).json({
-                            error: err
-                        });
-                      });
-                }
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              userName: req.body.userName,
+              email: req.body.email,
+              password: hash,
             });
-        }
-    })
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
 });
 
 module.exports = router;
