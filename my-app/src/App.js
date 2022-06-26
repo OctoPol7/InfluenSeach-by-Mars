2b8ddc9ae7f1c0519e391359461988aeaca13424
OctoPol7 <thechumpower@gmail.com>
2022-06-26 16:07:36 -0700

    feat: routing pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import SignUp from "./SignUp/SignUp.js";
 import SearchPage from "./SearchPage/SearchPage.js";
 import LogIn from "./LogIn/LogIn.js";
@@ -9,13 +10,29 @@ import SingleCampaign from './SingleCampaign/SingleCampaign.js'
 const App = (props) => {
   return (
     <div className="App">
-      {/* <SearchPage /> */}
-      {/* <SignUp />
-      <LogIn />*/}
+      <Route path="/search">
+        <SearchPage />
+      </Route>
 
-      {/* <SearchResult />  */}
-      {/* <GetSearch /> */}
-      <SingleCampaign />
+      <Route path="/signup">
+        <SignUp />
+      </Route>
+
+      <Route path="/login">
+        <LogIn />
+      </Route>
+
+      <Route path="/searchresult">
+        <SearchResult />
+      </Route>
+
+      <Route path="/getserach">
+        <GetSearch />
+      </Route>
+
+      <Route path="/singlecampaign">
+        <SingleCampaign />
+      </Route>
     </div>
   );
 };
