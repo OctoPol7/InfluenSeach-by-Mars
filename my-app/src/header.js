71df3d5ce283d7c4a6a0c807f6f832d22cd078f3
tanyag183 <ttanya01@mylangara.ca>
2022-07-12 02:20:33 -0700

    Logo added

--- my-app/src/header.js
+++ my-app/src/header.js
@@ -1,13 +1,13 @@
 
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import Logo from './Logo.svg'
 
 const Header = props => {
     return (
       <div className='header-background'>
       <div className="header header-max-width-div">
-        {/* Add logo here */}
-        <h1>InfluenSearch</h1>
+        <img className='logo' src={Logo}></img>
         <ul className="nav">
           <li>
             <NavLink to="/search">Search</NavLink>
