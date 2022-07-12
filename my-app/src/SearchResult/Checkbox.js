7ce907b6f73cf2c42b0b821c6ef08588c4614192
OctoPol7 <thechumpower@gmail.com>
2022-07-12 11:22:25 -0700

    feat: checkbox

--- my-app/src/SearchResult/Checkbox.js
+++ my-app/src/SearchResult/Checkbox.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Checkbox = props => {
+    const grabValue = (e) => {
+        // let isChecked = e.target.checked;
+        // console.log(e);
+        props.checkboxHandler(e);
+    }
     return(
         <>
             <div>
-                <input type="checkbox" id={props.name} name={props.name} />
+                <input type="checkbox" id={props.name} value={props.name} onChange={e => grabValue(e)} />
                 <label htmlFor={props.name}>{props.name}</label>
             </div>
         </>
