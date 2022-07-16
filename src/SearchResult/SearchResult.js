3a38290ef6b1840ed4fab42424e249259a12892b
OctoPol7 <thechumpower@gmail.com>
2022-07-15 19:11:53 -0700

    add: userName to header.js

--- src/SearchResult/SearchResult.js
+++ src/SearchResult/SearchResult.js
@@ -35,7 +35,7 @@ const SearchResult = props => {
 
     return (
       <div className="search-result">
-        <Header />
+        <Header userData={props.userData} />
 
         <div className="filters">
           <form onSubmit={addHandler}>
@@ -46,87 +46,86 @@ const SearchResult = props => {
               value={keyword}
               onChange={keywordHandler}
             />
-            <button type="submit" className='add-button'>
+            <button type="submit" className="add-button">
               Add
             </button>
           </form>
-            </div>
+        </div>
 
         <div className="searched-keywords">
-        <div className='max-width-div'>
+          <div className="max-width-div">
             <h2>Your current search:</h2>
-            <div className='one-line'>
-            <ul>
+            <div className="one-line">
+              <ul>
+                {props.keywordArray.map((key) => (
+                  <Tag tagname={key} />
+                ))}
 
-              {props.keywordArray.map((key) => (
-                <Tag tagname={key} />
-              ))}
-
-              <Tag tagname={JSON.parse(props.location).country} />
-            </ul>
-            <ResetInput
-              keywordArray={props.keywordArray}
-              location={props.location}
-              setKeywordArray={props.setKeywordArray}
-              setLocation={props.setLocation}
-            />
-        </div>
-        </div>
+                <Tag tagname={JSON.parse(props.location).country} />
+              </ul>
+              <ResetInput
+                keywordArray={props.keywordArray}
+                location={props.location}
+                setKeywordArray={props.setKeywordArray}
+                setLocation={props.setLocation}
+              />
+            </div>
           </div>
-        <div className='apply-filters-div max-width-div'>
-        <div className="filters">
-          <h2>Filters</h2>
-          <div className='choose-area'>
-            <h3>Geographic Areas</h3>
-            <CountryDropdown
-              location={props.location}
-              setLocation={props.setLocation}
-              searchPhrase={searchPhrase}
-              grabResults={grabResults}
-            />
-          </div>
-          {/* <div className='choose-gender'>
+        </div>
+        <div className="apply-filters-div max-width-div">
+          <div className="filters">
+            <h2>Filters</h2>
+            <div className="choose-area">
+              <h3>Geographic Areas</h3>
+              <CountryDropdown
+                location={props.location}
+                setLocation={props.setLocation}
+                searchPhrase={searchPhrase}
+                grabResults={grabResults}
+              />
+            </div>
+            {/* <div className='choose-gender'>
             <h3>Gender</h3>
             <Checkbox name="Male" />
             <Checkbox name="Female" />
             <Checkbox name="Transgender" />
             <Checkbox name="Non-Binary" />
           </div> */}
-          <div className='choose-topic'>
-            <h3>Topics</h3>
-            <Checkbox name="Lifestyle" />
-            <Checkbox name="Music" />
-            <Checkbox name="Family" />
-            <Checkbox name="Technology" />
-            <Checkbox name="Design" />
-            <Checkbox name="Travel" />
-          </div>
-        </div>
-
-        <div className='results-container'>
-          <GetSearch
-            searchPhrase={searchPhrase}
-            location={props.location}
-            grabResults={grabResults}
-          />
-          <div className="sort-by-grid">
-            <h2>{results.length} creators found</h2>
-            <SortByDropdown />
+            <div className="choose-topic">
+              <h3>Topics</h3>
+              <Checkbox name="Lifestyle" />
+              <Checkbox name="Music" />
+              <Checkbox name="Family" />
+              <Checkbox name="Technology" />
+              <Checkbox name="Design" />
+              <Checkbox name="Travel" />
+            </div>
           </div>
 
-          <div className="results">
-            {results.map((result) => (
-              <InfluCard
-                sub_count={result.statistics.subscriberCount}
-                video_count={result.statistics.videoCount}
-                influ_name={result.brandingSettings.channel.title}
-                influ_img={result.snippet.thumbnails.default.url}
-                topic_ids={result.topicDetails.topicIds}
-              />
-            ))}
+          <div className="results-container">
+            <GetSearch
+              searchPhrase={searchPhrase}
+              location={props.location}
+              grabResults={grabResults}
+            />
+            <div className="sort-by-grid">
+              <h2>{results.length} creators found</h2>
+              <SortByDropdown />
+            </div>
+
+            <div className="results">
+              {results.map((result) => (
+                <InfluCard
+                  sub_count={result.statistics.subscriberCount}
+                  video_count={result.statistics.videoCount}
+                  influ_name={result.brandingSettings.channel.title}
+                  influ_img={result.snippet.thumbnails.default.url}
+                  topic_ids={result.topicDetails.topicIds}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
       </div>
     );
 }
