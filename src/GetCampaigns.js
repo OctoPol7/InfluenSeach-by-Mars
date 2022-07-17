625d2e738c5070e4d220a13962cd1400b5bb336b
Roberto Guevara <robertoeguevara@gmail.com>
2022-07-17 14:56:04 -0700

    component to get all campaigns

---
+++ src/GetCampaigns.js
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const GetCampaigns = (props) => {
+  const [isLoading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    async function getCampaigns() {
+      const uid = props.userData.uid;
+      const token = props.userData.token;
+      const url = `http://localhost:4000/campaigns/${uid}`;
+
+      await axios
+        .get(url,
+          {
+            headers: {
+              'Authorization': 'Bearer '+ token
+            }
+          }
+        )
+        .then((resData) => {
+          console.log(resData);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    getCampaigns();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  //console.log(search);
+  if (isLoading) {
+    return (
+      <>
+        <div>Loading...</div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        {/* <h1>Search Successfull!</h1> */}
+      </>
+    );
+  }
+};
+
+export default GetCampaigns;
