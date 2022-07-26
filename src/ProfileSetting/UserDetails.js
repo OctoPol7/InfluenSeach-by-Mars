4060be3e95ddf9892fc1dbb4d69628eb7d9b95d9
OctoPol7 <thechumpower@gmail.com>
2022-07-25 18:42:58 -0700

    add: show username on profilesetting page

--- src/ProfileSetting/UserDetails.js
+++ src/ProfileSetting/UserDetails.js
@@ -4,7 +4,7 @@ import React from 'react'
 const UserDetails = props => {
     return<>
         <div className='user-details'>
-        <h2>Name </h2>
+        <h2>{props.userData ? props.userData.userName : "USERNAME"}</h2>
         <p>Pro User</p>
         <p>Joined on May 2021</p>
         </div>
